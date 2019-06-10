66b55f7ff4efd40572f457a55d36d0789a982bf0
Katharine Cooney <katharinecooney@gmail.com>
2019-06-10 12:35:44 +0200

    Add extra CSS styles to clock hands

--- clock.js
+++ clock.js
@@ -1,6 +1,7 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const hands = document.querySelectorAll('.hand');
 
 function setDate() {
   const now = new Date();
@@ -8,17 +9,20 @@ function setDate() {
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  // console.log(seconds);
 
   const minutes = now.getMinutes();
   const minutesDegrees = ((minutes / 60) * 360) + 90;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-  // console.log(minutes);
 
   const hours = now.getHours();
   const hoursDegrees = ((hours / 12) * 360) + 90;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`
-  // console.log(hours);
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  if (secondsDegrees === 450 || minutesDegrees === 450 || hoursDegrees === 450) {
+    hands.forEach(hand => {
+      hand.style.transition = 'none';
+    })
+  }
 }
 
 setInterval(setDate, 1000);
