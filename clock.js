4863f0a3323a9d802fef1f58659355deb2d85551
Katharine Cooney <katharinecooney@gmail.com>
2019-06-10 13:02:04 +0200

    Add if/else to prevent 360deg transition at the start of clock rotation

--- clock.js
+++ clock.js
@@ -1,28 +1,33 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
-const hands = document.querySelectorAll('.hand');
+const allHands = document.querySelectorAll('.hand');
 
-function setDate() {
+const moveClockHands = () => {
   const now = new Date();
 
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const minutes = now.getMinutes();
   const minutesDegrees = ((minutes / 60) * 360) + 90;
-  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const hours = now.getHours();
   const hoursDegrees = ((hours / 12) * 360) + 90;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
-  if (secondsDegrees === 450 || minutesDegrees === 450 || hoursDegrees === 450) {
-    hands.forEach(hand => {
+  if(secondsDegrees === 90 || minutesDegrees === 90 || hoursDegrees === 90) {
+    allHands.forEach(hand => {
       hand.style.transition = 'none';
     })
+  } else {
+    allHands.forEach(hand => {
+      hand.style.transition = '';
+    })
   }
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
-setInterval(setDate, 1000);
+setInterval(moveClockHands, 1000);
